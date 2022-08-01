cfb5adc719d24eb3856dd7d18bb36a569e824911
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-08-01 16:23:58 +0530

    Add files via upload

---
+++ functions2.py
@@ -0,0 +1,38 @@
+# def gm():
+# '''Author:Python 9am
+# the time is now 9:22am
+# we are doing functions topic'''
+# print(gm._doc_)
+# def info(name="hey",age=45):
+#     print("name:",name)
+#     print("age:",age)
+# info()
+# info('py')
+# info('rav',25)
+# info(name='gopal')
+# info(age=23)
+# addition of two numbers using functions
+# def s(a,b):
+#     c=a+b
+#     d=a+b
+#     print('sum is',c)
+#     print('sum is',d)
+# s(10,20)
+# s(10.5,2.3)
+# def sum(a,b):
+#     c=a+b
+#     return c
+# x=sum(10,20)
+# print('sum is',x)
+# y=sum(10.5,2.3)
+# print('sum is:',y)
+# ''' even odd'''
+# def eo(num):
+#     if num%2==0:
+#         print(num,'is even')
+#     else:
+#         print(num,"is odd")
+# n=int(input())
+# eo(n)
+# eo(12)
+# eo(13)
